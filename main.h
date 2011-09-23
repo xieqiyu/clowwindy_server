1a93975b7d202c6cc3907a80be4834435f85b91c
clowwindy <kizuna@hoshimi.me>
2011-09-23 14:15:27 +0800

    change name

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 
-#define MAXEVENTS	4096
-#define MAX_PORCESS	2048
+#define MAXEVENTS	40960
+#define MAX_PORCESS	20480
 #define BUF_SIZE	4096
 
 #define MAX_URL_LENGTH	128
@@ -11,7 +11,7 @@
 
 #define USE_SENDFILE 1
 
-struct process_t {
+struct process {
     int sock;
     int status;
     int response_code;
@@ -22,25 +22,25 @@ struct process_t {
     char buf[BUF_SIZE];
 };
 
-void send_response_header(struct process_t *process);
+void send_response_header(struct process *process);
 
 int setNonblocking(int fd);
 
-struct process_t* find_process_by_sock(int sock);
+struct process* find_process_by_sock(int sock);
 
-struct process_t* accept_sock(int listen_sock);
+struct process* accept_sock(int listen_sock);
 
-void read_request(struct process_t* process);
+void read_request(struct process* process);
 
-void send_response_header(struct process_t *process);
+void send_response_header(struct process *process);
 
-void send_response(struct process_t *process);
+void send_response(struct process *process);
 
-void cleanup(struct process_t *process);
+void cleanup(struct process *process);
 
-void handle_error(struct process_t *process, char* error_string);
+void handle_error(struct process *process, char* error_string);
 
-void reset_process(struct process_t *process);
+void reset_process(struct process *process);
 
 int open_file(char *filename);
 
